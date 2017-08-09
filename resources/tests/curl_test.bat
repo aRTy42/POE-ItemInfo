4d9991fd8982130774c4251936a14f0b0419b8f9
Manuel Schermuly <prinzeru@gmail.com>
2017-08-09 02:39:41 +0200

    improved curl_test.bat

--- resources/tests/curl_test.bat
+++ resources/tests/curl_test.bat
@@ -9,12 +9,15 @@ SET useragent=Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML
 SET cfduid=
 SET cfClearance=
 
-SET outDir=%cd%\tempOutput
-if not exist "%outDir%" mkdir "%outDir%"
+SET workDir=%~dp0
+SET outDir="%workDir%tempOutput"
+SET outFile="%workDir%tempOutput\outresult.txt"
+if not exist %outDir% mkdir %outDir%
 del %outDir%\outresult.txt
+cd %workDir%
 cd ..\..
 cd lib
 
 @echo on
 echo(
-%cd%\curl.exe -ILks -H "User-Agent: %useragent%" -H "Cookie: __cfduid= %cfduid%; cf_clearance= %cfClearance%" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Accept-Encoding:gzip, deflate" -H "Accept-Language:de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4" -H "Connection:keep-alive" -H "Host:poe.trade" -H "Upgrade-Insecure-Requests:1" "http://poe.trade" >> %outDir%\outresult.txt
+"%cd%\curl.exe" -ILks -H "User-Agent: %useragent%" -H "Cookie: __cfduid= %cfduid%; cf_clearance= %cfClearance%" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Accept-Encoding:gzip, deflate" -H "Accept-Language:de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4" -H "Connection:keep-alive" -H "Host:poe.trade" -H "Upgrade-Insecure-Requests:1" "http://poe.trade" >> %outFile%
