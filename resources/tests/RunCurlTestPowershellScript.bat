ac7ae8f78cadc69bfcfca48ae9dfca9881d83f1e
Manuel Schermuly <prinzeru@gmail.com>
2017-08-14 19:35:15 +0200

    run curl test as admin

--- resources/tests/RunCurlTestPowershellScript.bat
+++ resources/tests/RunCurlTestPowershellScript.bat
@@ -1,1 +1,2 @@
-start powershell -command "& '.\curl_test.ps1'"
+SET workDir=%~dp0
+start powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -file %workDir%\curl_test.ps1' -verb RunAs}"
