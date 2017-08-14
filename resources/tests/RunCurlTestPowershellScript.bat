94bacf62dab999bb29cdc2123cf00265e998d8d0
Manuel Schermuly <prinzeru@gmail.com>
2017-08-14 19:38:52 +0200

    add "-executionpolicy remotesigned" to run ps script command

--- resources/tests/RunCurlTestPowershellScript.bat
+++ resources/tests/RunCurlTestPowershellScript.bat
@@ -1,2 +1,2 @@
 SET workDir=%~dp0
-start powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -file %workDir%\curl_test.ps1' -verb RunAs}"
+start powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -executionpolicy remotesigned -file %workDir%\curl_test.ps1' -verb RunAs}"
