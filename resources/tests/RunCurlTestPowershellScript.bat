eb79b65a0b4833a670563c6ed5721ce85d8ad9d9
Manuel Schermuly <prinzeru@gmail.com>
2017-08-16 21:07:18 +0200

    powershell execution policy stuff

--- resources/tests/RunCurlTestPowershellScript.bat
+++ resources/tests/RunCurlTestPowershellScript.bat
@@ -1,2 +1,2 @@
 SET workDir=%~dp0
-start powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -executionpolicy remotesigned -file %workDir%\curl_test.ps1' -verb RunAs}"
+start PowerShell -NoProfile -ExecutionPolicy UnRestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy UnRestricted -File %workDir%\curl_test.ps1' -Verb RunAs}"
